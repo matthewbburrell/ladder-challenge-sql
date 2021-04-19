6476f0364f10eaccd79e10be74b5cecd623811df
MattBurr77 <you@example.com>
2021-04-19 13:12:15 -0700

    finished view

--- 03-view.sql
+++ 03-view.sql
@@ -17,12 +17,41 @@ select substr(date, 1, 4) as 'Year',
 	avg(low) as 'Average Low', 
 	avg(close) as 'Average Close', 
 	sum(volume) as 'Total Volume'
-from yum;
+from yum
+group by Year, Month;
 
 --64) Save the results of the previous query as a view named `yum_by_month`.
+
+create view if not exists yum_by_month
+as
+select substr(date, 1, 4) as 'Year',
+	substr(date,6,2) as 'Month', 
+	avg(open) as 'Average Open', 
+	avg(high) as 'Average High', 
+	avg(low) as 'Average Low', 
+	avg(close) as 'Average Close', 
+	sum(volume) as 'Total Volume'
+from yum
+group by Year, Month;
 
 
 --65) Create a view of `transactions` consisting of only three columns: year, month, and total sales in that month. Call this view `trans_by_month`.
 
+create view if not exists trans_by_month
+as
+select substr(orderdate, 1, 4) as 'Year',
+	substr(orderdate, 6, 2) as 'Month',
+	sum(unit_price * quantity) as 'Total_Sales'
+from transactions
+group by Year, Month;
 
 --66) Create a view of `transactions` consisting of only two columns: `employee_id` and the total sales corresponding to that employee. Call this view `trans_by_employee`.
+
+create view if not exists trans_by_employee
+as
+select employee_id, 
+	sum(unit_price * quantity) as 'Total_Sales'
+from transactions
+group by employee_id;
+
+
